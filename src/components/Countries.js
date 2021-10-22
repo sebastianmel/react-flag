34236143f9065ca8fd3a5a4faca4b731efc625fe
sebastianmel <57873668+sebastianmel@users.noreply.github.com>
2021-10-22 16:30:42 +0200

    add
    ajout d'axios premières requetes

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Countries = () => {
-    const [data,setData] = useState([]);
+    const [data, setData] = useState([]);
 
-    return(
-    <div>
+    useEffect(() => {
+        axios
+            .get(`https://restcountries.com/v2/all?fields=name,population,region,capital,flag`
+            )
+            .then((res) => setData(res.data));
+
         
-    </div>);
+    }, [])
+    console.log(data);
+
+
+    return (
+        <div className="countries">
+            <ul className="countries-list">
+                {data.map((country) =>(
+                   <li>{country.name}</li> 
+                ))}
+            </ul>
+
+        </div>
+    );
 };
 
 export default Countries;
